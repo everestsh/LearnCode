2cdb57baffea042686273b66eef4bfc67cc24151
Mingjun Geng <sooofeve@gmail.com>
2022-02-25 23:56:12 -0800

    daily Practice For 02225022 01 : rust

---
+++ rust/test/0001/hello-rust/target/release/deps/hello_rust-746cd606c443eb2a.d
@@ -0,0 +1,5 @@
+/Users/raylea/Dropbox/Workspace/github/everestsh/LearnCode/rust/test/0001/hello-rust/target/release/deps/hello_rust-746cd606c443eb2a: src/main.rs
+
+/Users/raylea/Dropbox/Workspace/github/everestsh/LearnCode/rust/test/0001/hello-rust/target/release/deps/hello_rust-746cd606c443eb2a.d: src/main.rs
+
+src/main.rs:
